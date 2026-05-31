4333d9fe114885a77324ccf122b904456a7100e6
Prashant Singh <prashantsingh1204@gmail.com>
2026-05-31 23:42:17 +0530

    Create outputs.tf

---
+++ modules/aws/networking/environment/dev/outputs.tf
@@ -0,0 +1,49 @@
+output "vpc_id" {
+  description = "ID of the created VPC."
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_id" {
+  description = "ID of the first public subnet."
+  value       = aws_subnet.public[0].id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets."
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_id" {
+  description = "ID of the first private subnet."
+  value       = aws_subnet.private[0].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets."
+  value       = aws_subnet.private[*].id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the internet gateway."
+  value       = aws_internet_gateway.this.id
+}
+
+output "nat_eip_id" {
+  description = "ID of the NAT Gateway Elastic IP."
+  value       = aws_eip.nat.id
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT gateway."
+  value       = aws_nat_gateway.this.id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table."
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "ID of the private route table."
+  value       = aws_route_table.private.id
+}
